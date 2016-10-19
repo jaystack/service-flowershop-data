cde7617b290ab75bbdbf5768969b237a6b8dd73b
Peter Nochta <peter.nochta@jaydata.org>
2016-10-19 14:34:44 +0200

    fix service.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
 const pkg = require('./package.json');
-const app = require('./lib/index').default;
+const app = require('./lib/src/index').default;
 
 winston.info('Started', pkg.name);
