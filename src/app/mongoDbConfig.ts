bb3eef1003710fd62f49ab693a4a4fbaccfa1402
szabof-JS <szabof@jaystack.com>
2017-03-02 12:14:45 +0100

    Use 'localhost:port' style alias from system-endpoints.json

--- src/app/mongoDbConfig.ts
+++ src/app/mongoDbConfig.ts
@@ -2,7 +2,7 @@ export default function mongoDbConfig() {
   return {
     async start({ endpoints }) {
       const { getServiceAddress } = endpoints
-      const {host, port} = endpoints.getServiceEndpoint('dataServer')
+      const {host, port} = endpoints.getServiceEndpoint('localhost:3003')
       return {
         mongodb: {
           db: "flowershop"
